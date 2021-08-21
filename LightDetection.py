27ba440341992061b1dbc413a86ca839043aee01
Dylan Sutherland <46365659+Nalydsea@users.noreply.github.com>
2021-08-21 23:09:44 +1000

    Update LightDetection.py
    Implemented HTTP PUT requests for interfacing with Philips Hue light bulb

--- LightDetection.py
+++ LightDetection.py
@@ -1,26 +1,32 @@
 import RPi.GPIO as GPIO
-import time
+from time import sleep
+import requests
+
+# HTTP PUT data for light
+url = "http://192.168.1.114:80/api/6A69BD6601/lights/2/state"
+on_payload = "{\"on\":true}"
+off_payload = "{\"on\":false}"
+headers = {'Content-Type':'text/plain'}
 
 class LightDetection:
- 
- #GPIO set Mode
- GPIO.setwarnings(False)
- GPIO.setmode(GPIO.BCM)
+    #GPIO set Mode
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)
 
- #set GPIO Pins
- GPIO_TRIGGER = 18 
- GPIO_LED = 24
+    #set GPIO Pins
+    PIR = 18 
 
- #set GPIO direction
- GPIO.setup(GPIO_TRIGGER, GPIO.IN)
- GPIO.setup(GPIO_LED, GPIO.OUT)
- GPIO.output(GPIO_LED, GPIO.LOW)
+    #set GPIO direction
+    GPIO.setup(PIR, GPIO.IN)
 
- while True:
- #when motion detected turn on LED
-     if(GPIO.input(PIR_input)):
-         GPIO.output(LED, GPIO.HIGH)
-         #Time delay 10 minutes
-         time.sleep(600)
-     else:
-         GPIO.output(LED, GPIO.LOW)
+    while True:
+        #when motion detected turn on light
+        if(GPIO.input(PIR)):
+            r = requests.put(url, headers=headers, data=on_payload)
+            #Time delay 10 minutes
+            sleep(600)
+            #Uncomment this delay for testing
+            #sleep(5)
+        else:
+            #when no motion detected turn off light
+            r = requests.put(url, headers=headers, data=off_payload)
