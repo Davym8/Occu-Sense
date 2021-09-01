091e5f8d3a179ea15e2e327f2954353c68b59dd9
DJ2507 <80818163+DJ2507@users.noreply.github.com>
2021-09-02 00:05:54 +1000

    Update MotionDetection.py

--- MotionDetection.py
+++ MotionDetection.py
@@ -7,6 +7,7 @@ import json
 import Adafruit_DHT
 
 
+
 # HTTP PUT data for light
 url = "http://192.168.1.114:80/api/6A69BD6601/lights/2/state"
 on_payload = "{\"on\":true}"
@@ -31,6 +32,7 @@ off_payload2 = json.dumps({
 headers = {'Content-Type': 'application/json'}
 
 
+
 DHT_SENSOR = Adafruit_DHT.DHT22
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
@@ -42,8 +44,9 @@ GPIO.setup(DHT,GPIO.IN)
 
 SET_TEMP = 24
 
-def controlHVAC_on():
 
+
+def HVAC_on():
     hum, temp = Adafruit_DHT.read_retry(DHT_SENSOR, DHT)
 
     if temp is not None:
@@ -52,26 +55,30 @@ def controlHVAC_on():
         elif temp > SET_TEMP + 2:
             r = requests.put(url2, headers=headers, data=off_payload2)
 
+def HVAC_off:
+    r = requests.put(url2, headers=headers, data=off_payload2)
+
+def light_on():
+    r = requests.put(url, headers=headers, data=on_payload)
+    
+def light_off():
+    r = requests.put(url, headers=headers, data=off_payload)
+
+
 
 while True:
   i = GPIO.input(GPIO_PIR_Input)
-  
-  if i == 0:  
-    camera_input() #Double check with camera
-    if camera_input == False:
-      r = requests.put(url, headers=headers, data=off_payload) #Switch off light
-      r = requests.put(url2, headers=headers, data=off_payload2) #Switch off HVAC 
-      time.sleep(0.1)
+  if i == 0:
+    light_off() #Switch off light
+    HVAC_off() #Switch off HVAC 
+    time.sleep(15)
     
-    if camera_input == True:
-      i == 1 #Continue operations
-      time.sleep(0.1)
   time.sleep(0.1)
     
   if i == 1:
-    r = requests.put(url, headers=headers, data=on_payload) #Switch on light
-    controlHVAC_on() #Check if HVAC should be on or not
-    time.sleep(0.1)
+    light_on() #Switch on light
+    HVAC_on() #Check if HVAC should be on or not
+    time.sleep(15)
 
 #Can use 'from gpiozero import MotionSensor'
 #Allows 'pir.wait_for_motion()' and 'pir.wait_for_no_motion()'
